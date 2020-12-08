be6b489e63ad18aacbe222f19913a7d9b64641a7
Errforx@Wh4t3v3r <68257861+Errforx@users.noreply.github.com>
2020-12-08 18:35:45 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
 const output = document.getElementById('output');
 const type = document.getElementById('type');
 
-cordova.plugins.barcodeScanner.scan(
-    function (result) {
-        output.innerHTML = 'Result ::  ' + result.text;
-        type.innerHTML = 'Format ::  ' + result.format;
-    },
-    function (error) {
-        alert('Scanning Failed: ' + error);
-    }
-);
+window.addEventListener('click', successLoaded);
+
+function successLoaded() {
+    cordova.plugins.barcodeScanner.scan(
+        function (result) {
+            output.innerHTML = 'Result ::  ' + result.text;
+            type.innerHTML = 'Format ::  ' + result.format;
+        },
+        function (error) {
+            alert('Scanning Failed: ' + error);
+        },
+        {
+            preferFrontCamera : false, // iOS and Android
+            showFlipCameraButton : false, // iOS and Android
+            showTorchButton : false, // iOS and Android
+            torchOn: false, // Android, launch with the torch switched on (if available)
+            saveHistory: false, // Android, save scan history (default false)
+            prompt : "Scan Merchant QR Code", // Android
+            resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
+            formats : "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
+            orientation : "portrait", // Android only (portrait|landscape), default unset so it rotates with the device
+            disableAnimations : true, // iOS
+            disableSuccessBeep: true // iOS and Android
+        }
+    );
+};
